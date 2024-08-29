32b0b5723b41aebdeaf9ce4acd5b28f6c7790c5b
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-08-29 14:47:40 +0300

    Create ex4.py

---
+++ Basic/ex4.py
@@ -0,0 +1,6 @@
+# Remove first n characters from a string
+# Write a Python code to remove characters from a string from 0 to n and return a new string.
+
+def remove_chars(string,n):
+    stringSliced = string[n:]
+    print(stringSliced) 
